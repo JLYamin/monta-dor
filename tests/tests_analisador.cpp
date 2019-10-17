cd3f7df63fbb9240f15bd068518fe8933059aa01
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-10-17 08:32:14 -0300

    :recycle: Testes verdes após integração das branches

--- tests/tests_analisador.cpp
+++ tests/tests_analisador.cpp
@@ -302,8 +302,6 @@ TEST_CASE( "Valida linha", "[Sintático]" )
     REQUIRE( analisador_sintatico->monta_linha("COPY NEW_DATA,OLD_DATA+2") == "9 00 002");
   } // SECTION( "Monta linha com opcodes com dois argumentos")
 
-  }
-
   SECTION( "Monta linha com rótulo no começo" )
   {
     REQUIRE( analisador_sintatico->monta_linha("L1: DIV DOIS") == "4 00");
