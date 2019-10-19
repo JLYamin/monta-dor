9f919b261fb914173f22d42bd748f19dd14a97d1
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-10-19 13:57:31 -0300

    :sparkles: Main recebe argumentos pelo terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include "analisador.hpp"
 
 
 
-int main(void)
-{
-
+int main(int argc, char *argumentos_terminal[])
+{   
+    string nome_arquivo = argumentos_terminal[1];
+    string path_pasta_arquivos = ("Entradas ASM/");
+    string codigo_objeto;
+    Assembler* montador = new Assembler();
+    codigo_objeto = montador->monta_texto(path_pasta_arquivos+nome_arquivo);
+    cout << codigo_objeto << endl;
+    return 0;
 }
